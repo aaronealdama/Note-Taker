01e8f53c6cc5e1573632db2710290e69d46dcd8a
Aaron Ealdama <aaronealdama@gmail.com>
2020-03-07 18:40:05 -0600

    initial file creation and some work done

---
+++ assets/css/js/server.js
@@ -0,0 +1,36 @@
+/* HTML routes: 
+/notes = notes.html
+* = index.html
+
+API routes:
+GET - /api/notes = read db.json and return all saved notes as JSON
+POST - /api/notes = receive a new note to save on req body, add it to the db.json and return the new note to the client
+
+DELETE - /api/notes/:id = Should recieve a query param containing the id of the note to delete
+*/
+
+// Packages
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+// API routes
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
